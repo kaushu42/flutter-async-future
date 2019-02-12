eac97d38e980be55ef877afb399d218cbefed8b9
Kaushal <kaushu42@gmail.com>
2019-02-12 08:03:32 +0545

    Display items as list

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,53 @@
 import "package:flutter/material.dart";
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-void main(List<String> args) {
+void main() async {
+  List _data = await getJSON();
+
   runApp(
     MaterialApp(
-      home: MyWidget(),
+      home: new Scaffold(
+        body: new Center(
+          child: new ListView.builder(
+            itemCount: _data.length,
+            itemBuilder: (BuildContext context, int position) {
+              if (position.isOdd) {
+                return new Divider();
+              }
+
+              final index = position ~/ 2; // dividing position by 2 and returning an integer
+
+              return new ListTile(
+                title: new Text(
+                  '${_data[index]['title']}',
+                  style: new TextStyle(
+                    fontSize: 12.0,
+                  ),
+                ),
+                leading: CircleAvatar(
+                  backgroundColor: Colors.green,
+                  child: new Text(_data[index]['id'].toString()),
+                ),
+                subtitle: new Text(
+                  _data[index]['body'],
+                ),
+                onTap: () => debugPrint(
+                      "${_data[index]['id']}",
+                    ),
+              );
+            },
+            padding: const EdgeInsets.all(10.0),
+          ),
+        ),
+      ),
     ),
   );
 }
 
-class MyWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: new Center(
-        child: new Text(
-          "Hello world",
-        ),
-      ),
-    );
-  }
+Future<List> getJSON() async {
+  String url = 'https://jsonplaceholder.typicode.com/posts';
+  http.Response response = await http.get(url);
+  return jsonDecode(response.body);
 }
